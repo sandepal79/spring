4674d505011980aeb3afc65842eb7ef2d34c58a5
sandepal79 <sandepal79@gmail.com>
2022-06-15 22:56:01 +0200

    init emplist from faker mapping for get all or one, and post one

--- myrestapi/src/main/java/com/alex/springmvcboot/BusinessLogic.java
+++ myrestapi/src/main/java/com/alex/springmvcboot/BusinessLogic.java
@@ -1,6 +1,5 @@
 package com.alex.springmvcboot;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,46 +7,20 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class BusinessLogic {
-	
-	
-	
-	
-	
-/*	
+
 	@Autowired
-	Smartphone smph;
-	List<Smartphone> phoneList = new ArrayList<>();
-		
+	InitEmployees initemp;
+
 	public BusinessLogic() {
-		phoneList.add(new Smartphone("apple","iphone","12 pro","12MB", "256GB", 1000, "USD"));
-		phoneList.add(new Smartphone("apple","iphone","12 pro max","12MB", "256GB", 1200, "USD"));
-		phoneList.add(new Smartphone("apple","iphone","12 pro max","12MB", "512GB", 1500, "USD"));
-		phoneList.add(new Smartphone("apple","iphone","13 pro","14MB", "256GB", 1800, "USD"));
-		phoneList.add(new Smartphone("apple","iphone","13 pro max","14MB", "512GB", 2500, "USD"));
-		phoneList.add(new Smartphone("apple","iphone","13 pro max","14MB", "256GB", 2100, "USD"));
-		phoneList.add(new Smartphone("apple","iphone","11 pro max","10MB", "256GB", 900, "USD"));
-		
 	}
-	
-	public List<Smartphone> getAll(){
-		return phoneList;
+
+	public List<Employee> getEmpList() {
+		return initemp.getEmpList();
 	}
-	
-	public Smartphone getOne(){
-		return phoneList.get(0);
+
+	public void addEmp(Employee emp) {
+		initemp.empList.add(
+				new Employee(emp.getEmpId(), emp.getName(), emp.getJobTitle(), emp.getSalary(), emp.getBirthDate()));
 	}
-	
-	public void addItem(Smartphone smp){
-		phoneList.add(new Smartphone(smp.getCompany(), 
-				smp.getVariant(),
-				smp.getModel(),
-				smp.getCamerapix(),
-				smp.getStorage(),
-				smp.getPriceValue(),
-				smp.getPriceCurrency()));
-	}
-	
-*/
-	
-	
+
 }
--- myrestapi/src/main/java/com/alex/springmvcboot/Employee.java
+++ myrestapi/src/main/java/com/alex/springmvcboot/Employee.java
@@ -4,20 +4,27 @@ import java.sql.Date;
 
 import org.springframework.stereotype.Component;
 
+import com.github.javafaker.Faker;
+
 @Component
 public class Employee {
-	
+
 	private String empId;
-	private String name;	
+	private String name;
 	private String jobTitle;
 	private int salary;
 	private Date birthDate;
-	
-	
+
 	public Employee() {
-		
+		Faker faker = new Faker();
+		this.empId = faker.code().isbn13(false);
+		this.name = faker.name().name();
+		this.jobTitle = faker.job().title();
+		this.salary = (int) Math.round(Float.parseFloat(faker.commerce().price().replace(',', '.')) * 1000);
+		this.birthDate = new java.sql.Date(faker.date().birthday(25, 50).getTime());
+
 	}
-	
+
 	public Employee(String empId, String name, String jobTitle, int salary, Date date) {
 		this.empId = empId;
 		this.name = name;
@@ -25,42 +32,45 @@ public class Employee {
 		this.salary = salary;
 		this.birthDate = date;
 	}
-	
-	
-		
-	
+
 	public String getEmpId() {
 		return empId;
 	}
+
 	public void setEmpId(String empId) {
 		this.empId = empId;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getJobTitle() {
 		return jobTitle;
 	}
+
 	public void setJobTitle(String jobTitle) {
 		this.jobTitle = jobTitle;
 	}
+
 	public int getSalary() {
 		return salary;
 	}
+
 	public void setSalary(int salary) {
 		this.salary = salary;
 	}
+
 	public Date getBirthDate() {
 		return birthDate;
 	}
+
 	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
 	}
-	
-	
-	
-	
+
 }
